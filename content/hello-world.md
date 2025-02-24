de4206d5fadb20f845f44d9adaaf19a9d073f98d
R <rqou@berkeley.edu>
2025-02-24 00:21:41 +0000

    Add hello world

---
+++ content/hello-world.md
@@ -0,0 +1,5 @@
+Title: Hello world!
+Date: 2025-02-24
+Summary: Introductions are hard
+
+Hi! We're a small team working on curating information about interesting electronic components. Hope to have more soon!
--- pelicanconf.py
+++ pelicanconf.py
@@ -15,21 +15,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+# # Blogroll
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
+SOCIAL_WIDGET_NAME = "GitHub"
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("ArcaneNibble", "https://github.com/ArcaneNibble"),
+    ("jediminer543", "https://github.com/jediminer543"),
 )
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
